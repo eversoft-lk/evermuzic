8ac9af18b7e41b81f81aabfba9e747029eedcaad
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-06-13 03:58:42 +0530

    Home design

---
+++ components/Timeline/Item.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  time: String,
+  title: String,
+  artist: String,
+});
+</script>
+
+<template>
+  <li class="mb-10 ms-4">
+    <div
+      class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+    ></div>
+    <time
+      class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+      >{{ time }}</time
+    >
+    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+      {{ title }} - {{ artist }}
+    </h3>
+  </li>
+</template>
--- layouts/Home.vue
+++ layouts/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="w-full min-h-screen bg-[#131314] flex">
+  <div
+    class="w-full min-h-screen bg-gradient-to-b from-black to-[#0e1222] flex"
+  >
     <HomeNavbar />
     <div
-      class="flex-1 flex flex-col p-5 bg-cover bg-top bg-[url(/img/home-background.png)]"
+      class="flex-1 h-screen bg-cover bg-top bg-[url(/img/home-background.png)] overflow-hidden relative"
     >
-      <HomeHeader />
-      <slot />
+      <div class="w-full h-full flex flex-col overflow-y-scroll">
+        <HomeHeader />
+        <slot />
+      </div>
     </div>
   </div>
 </template>
